34fb3229a884238c25a2830d7e74cac1a7aa860a
Jatin.Kapoor <jatin.kapoor@target.com>
2018-06-23 01:56:54 -0500

    adding new mongo url

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static('public'));
 
 
 const db = require('./models');
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/webScrapper";
+const MONGODB_URI = process.env.MONGOLAB_JADE_URI || "mongodb://127.0.0.1:27017/webScrapper";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
